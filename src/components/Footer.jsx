87129080727947b97026bbfe9a18cde5625bc11f
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-06-14 17:19:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-yellow-500 shadow-md p-5 fixed bottom-0 w-full">
+    <footer className="bg-yellow-500 shadow-md p-3 mt-auto bottom-0 w-full">
       <div className="flex flex-row items-center justify-center">
-        <h1 className="text-white font-semibold text-xl">
+        <h1 className="text-white font-semibold text-xl ">
           Made By Jatin Bhandari 🧑‍💻 using React ⚛️🚀{" "}
         </h1>
       </div>
